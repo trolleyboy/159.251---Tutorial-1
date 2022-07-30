b88887cfca01060d0069f7b66e9389fe50b7c9c5
trolleyboy <44885244+trolleyboy@users.noreply.github.com>
2022-07-30 23:14:59 +1200

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("Greetings") + ", " + name;
+    return messages.getString("Greetings") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
